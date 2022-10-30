6cf8f9aeb14330b19269319c90b1dc8d6bb23c07
Christian <christian_ledesma_a@hotmail.com>
2022-10-30 04:11:14 +0100

    Project init

---
+++ src/main/java/Christian/Shopping/Api/Application.java
@@ -0,0 +1,13 @@
+package Christian.Shopping.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
